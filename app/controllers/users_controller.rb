073b2b08ef7a8b7bf7c8401b8a3e3d304adaf4fc
Shredder <shredder-rull@yandex.ru>
2015-05-15 09:20:11 +0600

    Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
   swagger_api :index do
     summary "Fetches all User items"
     notes "This lists all the active users"
+    param :query, :nested_id, :integer, :optional, "Team Id"
     param :query, :page, :integer, :required, "Page number"
-    param :path, :nested_id, :integer, :optional, "Team Id"
     response :unauthorized
     response :not_acceptable, "The request you made is not acceptable"
     response :requested_range_not_satisfiable
